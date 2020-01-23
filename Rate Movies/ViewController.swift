4c728d0c8a3b5cb0176fa48f49b7aaa022d9aa19
sudayn <sudosuwal@gmail.com>
2020-01-23 10:39:16 -0800

    first app commit

---
+++ Rate Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rate Movies
+//
+//  Created by Sudan Suwal on 11/3/1398 AP.
+//  Copyright © 1398 Sudan Suwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Rate MoviesTests/Rate_MoviesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Rate_MoviesTests.swift
+//  Rate MoviesTests
+//
+//  Created by Sudan Suwal on 11/3/1398 AP.
+//  Copyright © 1398 Sudan Suwal. All rights reserved.
+//
+
+import XCTest
+@testable import Rate_Movies
+
+class Rate_MoviesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
